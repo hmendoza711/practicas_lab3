4ee9e38a94ed6b4a9c60a4c81e2871f86d1d4b6e
Hector Mendoza <hectormaximendoza@gmail.com>
2024-05-19 14:59:31 -0300

    books_champions

--- base_vite_react/src/App.jsx
+++ base_vite_react/src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import BookItem from "./componentes/bookItem/BookItem";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  const books = [
+    {
+      bookTitle: "100 años de soledad",
+      bookAuthor: "Gabriel García Marquez",
+      bookRating: Array(5).fill("*"),
+      pageCount: 410,
+      imageUrl: "https://images.cdn3.buscalibre.com/fit-in/360x360/61/8d/618d227e8967274cd9589a549adff52d.jpg",
+    },
+    {
+      bookTitle: "Asesinato en el Orient Express",
+      bookAuthor: "Agatha Christie",
+      bookRating: Array(4).fill("*"),
+      pageCount: 256,
+      imageUrl: "https://m.media-amazon.com/images/I/71RFyM95qwL._AC_UF1000,1000_QL80_.jpg",
+    },
+    {
+      bookTitle: "Las dos torres",
+      bookAuthor: "J.R.R Tolkien",
+      bookRating: Array(5).fill("*"),
+      pageCount: 352,
+      imageUrl: "https://m.media-amazon.com/images/I/A1y0jd28riL._AC_UF1000,1000_QL80_.jpg",
+    },
+    {
+      bookTitle: "50 sombras de Grey",
+      bookAuthor: "E.L James",
+      bookRating: Array(1).fill("*"),
+      pageCount: 514,
+      imageUrl: "https://prodimage.images-bn.com/pimages/9781728260839_p0_v2_s1200x630.jpg",
+    },
+  ];
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <BookItem
+          title={books[0].bookTitle}
+          author={books[0].bookAuthor}
+          rating={books[0].bookRating}
+          pageCount={books[0].pageCount}
+          imageUrl ={books[0].imageUrl}
+        />
+        <BookItem
+          title={books[1].bookTitle}
+          author={books[1].bookAuthor}
+          rating={books[1].bookRating}
+          pageCount={books[1].pageCount}
+          imageUrl ={books[1].imageUrl}
+        />
+        <BookItem
+          title={books[2].bookTitle}
+          author={books[2].bookAuthor}
+          rating={books[2].bookRating}
+          pageCount={books[2].pageCount}
+          imageUrl ={books[2].imageUrl}
+        />
+        <BookItem
+          title={books[3].bookTitle}
+          author={books[3].bookAuthor}
+          rating={books[3].bookRating}
+          pageCount={books[3].pageCount}
+          imageUrl ={books[3].imageUrl}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ base_vite_react/src/componentes/bookItem/BookItem.jsx
@@ -0,0 +1,30 @@
+import { Card } from "react-bootstrap";
+import PropTypes from 'prop-types';
+
+
+const BookItem = ({ title, author, pageCount, rating, imageUrl }) => {
+
+    return (
+        <Card style={{ width: "22rem" }}>
+            <Card.Img
+                height={400}
+                variant="top"
+                src={imageUrl !== "" ? imageUrl : "https://bit.ly/47NylZk"}/>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Subtitle>{author}</Card.Subtitle>
+                <div>{rating?.length} estrellas</div>
+                <p>{pageCount} páginas</p>
+            </Card.Body>
+        </Card>
+    );
+};
+
+BookItem.propTypes = {
+    title: PropTypes.string,
+    author: PropTypes.string,
+    pageCount: PropTypes.number,
+    rating: PropTypes.array,
+};
+
+export default BookItem;
